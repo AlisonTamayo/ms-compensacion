0d5796bb90c4d46e594d925d0c3466eb9710503c
Alison Tamayo <aatamayo2@espe.edu.ec>
2026-01-18 22:30:48 -0500

    Sync final: Limpieza total y POMs corregidos

--- src/main/java/com/bancario/compensacion/CompensacionApplication.java
+++ src/main/java/com/bancario/compensacion/CompensacionApplication.java
@@ -6,11 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 public class CompensacionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompensacionApplication.class, args);
+	}
+
+	@Bean
+	public org.springframework.scheduling.TaskScheduler taskScheduler() {
+		return new org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler();
 	}
 
 }
